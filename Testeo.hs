23e2aeda6303c45b51d3da21ee7c36dfa03c20ca
Alumno <alumno@labsistemas.dom>
2018-04-06 12:00:50 -0300

    Ejercicio de mail tercera clase con DATA (struct)

---
+++ Testeo.hs
@@ -0,0 +1,6 @@
+
+email servidor (UnaPersona nombre apellido añoNacimiento) = nombre ++ apellido ++ show añoNacimiento ++ "@" ++ servidor ++ ".com"
+
+-- Struct Persona:  Nombre Apellido AñoDeNacimiento
+data Persona = UnaPersona String String Int deriving (Show)
+personaRejuvenecida años (UnaPersona nombre apellido añoNacimiento) = UnaPersona nombre apellido (añoNacimiento + años)
